c43042e837eed68e7f92ca4786872cdc218ea05d
OrlandoGarciaOlguin <90282471+OrlandoGarciaOlguin@users.noreply.github.com>
2021-09-27 17:53:41 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+//var n1= 10;
+//var n2 = 20;
+
+//if(n1 < n2){
+    //console.log("n1 es menor n2");
+//}else{
+   // console.log("n1 mayor n2");
+   // test = suma();
+//}
+//n1 = 100;
+
+//console.log(n1);
+//console.log(test);
+
+//function suma(){    
+    //let resultado = n1+ n2;
+    //return resultado;
+//}
+
+// var elementos = [];
+// var elementos2 = Array();
+// var elementos3 = ["hola",1,8.5,true,5,7,8,9,10];
+// var elementos4 = [];
+// var palabra = "Hola Mundo"
+// var contenedor = [];
+// var ejemplo = ["H","o","l","a"," ","M","u","n","d","o"];
+// for(let i = 0; i < elementos3.length; i++){
+//     console.log(elementos3[i]);
+//     elementos4.push(elementos3[i]);    
+// }
+// console.log(elementos4);
+
+// for(let i = 0; i < palabra.length; i++){        
+//         contenedor.push(palabra[i]);
+// }
+// console.log(contenedor);
+
+ // -- Arreglos de clave valor --
+var persona = {
+    "nombre" : "Orlando",
+    "apellidoP" : "Garcia",
+    "apellidoM" : "Olguin",
+    "edad" : 19,
+    "lenguajes":{
+        "lenguaje1" : "JavaScript",
+        "lenguaje2" : "Python",
+        "lenguaje3" : "C#"        
+    },
+    "Educacion":{
+        "Universidad" : "Universidad Politecnica de Tecamac",
+        "Carrera" : "Ingenieria en Software",
+        "Cuatrimestre" : "4to Cuatrimestre"        
+    },
+    "música": {
+        "cancion1" : "Creep",
+        "cancion2" : "I knew you were trouble",
+        "cancion3" : "Cardigan"        
+    }
+}
+console.log(persona);
